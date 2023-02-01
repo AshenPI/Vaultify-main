bbca1cff54d11363f3eee04e34e3818896ca4772
Abdulrahman375 <abdullrahman.abdu75@gmail.com>
2023-02-01 20:06:22 +0300

    ignore

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,56 +5,38 @@ const router = express.Router();
 const users = require("../data/users");
 const cry = require("../data/users");
 const bcrypt = require("bcrypt");
-const {cr} = require("../data/users");
+const { cr } = require("../data/users");
 
-router.post("/login", async (req, res , next) => {
+router.post("/login", async (req, res, next) => {
   try {
     const user = await UserModel.findOne({
       userName: req.body.userName,
-     
     });
 
-
-      // if(!user){
-      //   return res.status(400).json({message:"Login failed"})
-      // }
-      // if(!bcrypt.compareSync(req.body.password) , UserModel.password){
-      //  return res.status(400).json({message:" login failed"})
-      // }
-    
-      
     if (user) {
-      if( await bcrypt.compare(req.body.password  , user.password)){
-      res.send(user);
-      }else{
-        res.status(400).json( {message: "login failed"})
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        res.send(user);
+      } else {
+        res.status(400).json({ message: "login failed" });
       }
     }
 
-
     const cashier = await CashierModel.findOne({
       userName: req.body.userName,
-      
-     
-    })
+    });
 
-     if(cashier){
-      if( await bcrypt.compare(req.body.password  , cashier.password)){
-       
-      res.send(cashier);
-    }else {
-      res.status(400).json({ message: "Login failed" });
-     
+    if (cashier) {
+      if (await bcrypt.compare(req.body.password, cashier.password)) {
+        res.send(cashier);
+      } else {
+        res.status(400).json({ message: "Login failed" });
+      }
     }
-
-  }
   } catch (error) {
-
     res.status(400).json(error);
   }
-  next()
+  next();
 });
-
 
 router.post("/register", async (req, res) => {
   try {
