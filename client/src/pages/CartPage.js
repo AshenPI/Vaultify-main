a4173315dc80b1cf6b166dc7824e36f34bd4e672
Abdulrahman375 <abdullrahman.abdu75@gmail.com>
2023-02-02 23:06:52 +0300

    fix default value cart

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -164,11 +164,11 @@ function CartPage() {
             </Form.Item>
 
             <Form.Item  required name="customerName" label="Customer Name" >
-              <Input placeholder="Customer name" />
+              <Input defaultValue={"."} placeholder="Customer name" />
             </Form.Item>
 
             <Form.Item  required name="customerPhoneNumber" label="Phone Number">
-              <Input  placeholder="05*********" />
+              <Input defaultValue={"."} placeholder="05*********" />
             </Form.Item>
 
             <Form.Item  required name="paymentMode" label="Payment Mode">
@@ -235,10 +235,10 @@ function CartPage() {
             </Form.Item>
 
             <Form.Item  required name="customerName" label="Customer Name" >
-              <Input placeholder="Customer name" />
+              <Input defaultValue={"."} placeholder="Customer name" />
             </Form.Item>
 
-            <Form.Item  required name="customerPhoneNumber" label="Phone Number">
+            <Form.Item defaultValue={"."}  required name="customerPhoneNumber" label="Phone Number">
               <Input placeholder="05*********" />
             </Form.Item>
 
